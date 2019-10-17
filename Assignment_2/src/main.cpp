8dad65c8c17a1a80fdedf83f1438d12e6751894d
Daniel <daniel.bin.lu@gmail.com>
2019-10-17 14:59:46 -0400

    two of each buffer, array object, and matrix for lines and triangles

--- Assignment_2/src/main.cpp
+++ Assignment_2/src/main.cpp
@@ -23,21 +23,42 @@ using namespace Eigen;
 #include <iostream>
 
 // VertexBufferObject wrapper
-VertexBufferObject VBO;
+VertexBufferObject line_VBO;
 VertexBufferObject tri_VBO;
 
 // Contains the vertex positions
-Eigen::MatrixXf V(2,1);
+Eigen::MatrixXf line_V(2,1);
 Eigen::MatrixXf tri_V(2,1);
 
 bool tri_first = true;
 bool tri_complete = false;
 bool tri_insert_mode = false;
+int click_count = 0;
 //int num_triangles = 0;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    //The callback functions receives the cursor position
+    //measured in screen coordinates but relative to the top-left corner of the window content area.
+    if(tri_insert_mode == true)
+    {
+        // Get the size of the window
+        int width, height;
+        glfwGetWindowSize(window, &width, &height);
+
+        // Convert screen position to world coordinates
+        double xworld = ((xpos/double(width))*2)-1;
+        double yworld = (((height-1-ypos)/double(height))*2)-1; // NOTE: y axis is flipped in glfw
+
+        //add cursor position to line matrix bound to buffer object
+
+        line_VBO.update(line_V);
+    }
 }
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
@@ -60,9 +81,9 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
         if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
         {
             //if it's not the first vertex, resize the matrix to allow one more vertex to be added
-            if(!tri_first)
+            if(click_count>0)
             {
-                V.conservativeResize(NoChange ,V.cols()+1);
+                line_V.conservativeResize(NoChange ,line_V.cols()+1);
             }
             else
             {
@@ -70,17 +91,21 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
                 tri_first = false;
             }
 
+            //if it's the first click add a vertex
+            //then draw a segment from first click to wherever the mouse cursor is
+
+
             //add vertex
-            V.col(V.cols()-1) << xworld, yworld;
+            line_V.col(line_V.cols()-1) << xworld, yworld;
             
             //if V's number of columns is less than 3
-            if(V.cols()%4==0)
+            if(line_V.cols()%4==0)
             {
                 //check if the last click is relatively close to first vertex
-                Vector2f vertex0 = V.col(V.cols()-4);
-                Vector2f vertex2 = V.col(V.cols()-1);
-                float dist = (vertex0.coeffRef(0)-vertex2.coeffRef(0))*(vertex0.coeffRef(0)-vertex2.coeffRef(0)) + (vertex0.coeffRef(1)-vertex2.coeffRef(1))*(vertex0.coeffRef(1)-vertex2.coeffRef(1));
-                std::cout << dist << std::endl;
+                Vector2f vertex0 = line_V.col(line_V.cols()-4);
+                Vector2f vertex2 = line_V.col(line_V.cols()-1);
+                float dist = (vertex0.coeffRef(0)-vertex2.coeffRef(0))*(vertex0.coeffRef(0)-vertex2.coeffRef(0)) 
+                + (vertex0.coeffRef(1)-vertex2.coeffRef(1))*(vertex0.coeffRef(1)-vertex2.coeffRef(1));
                 if(dist<0.0003)
                 {
                     //now glDrawArrays draws triangle instead
@@ -92,7 +117,7 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
         }
 
         // Upload the change to the GPU
-        VBO.update(V);
+        line_VBO.update(line_V);
         //tri_VBO.update(tri_V);
     }
 }
@@ -110,7 +135,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     }
 
     // Upload the change to the GPU
-    VBO.update(V);
+    line_VBO.update(line_V);
 }
 
 int main(void)
@@ -170,9 +195,9 @@ int main(void)
     // attributes are stored in a Vertex Buffer Object (or VBO). This means that
     // the VAO is not the actual object storing the vertex data,
     // but the descriptor of the vertex data.
-    VertexArrayObject VAO;
-    VAO.init();
-    VAO.bind();
+    VertexArrayObject line_VAO;
+    line_VAO.init();
+    line_VAO.bind();
 
     VertexArrayObject tri_VAO;
     tri_VAO.init();
@@ -180,9 +205,9 @@ int main(void)
 
     // Initialize the VBO with the vertices data
     // A VBO is a data container that lives in the GPU memory
-    VBO.init();
-    V.resize(2,1);
-    VBO.update(V);
+    line_VBO.init();
+    line_V.resize(2,1);
+    line_VBO.update(line_V);
 
     tri_VBO.init();
     tri_V.resize(2,1);
@@ -217,13 +242,16 @@ int main(void)
     // The vertex shader wants the position of the vertices as an input.
     // The following line connects the VBO we defined above with the position "slot"
     // in the vertex shader
-    program.bindVertexAttribArray("position",VBO);
+    program.bindVertexAttribArray("position",line_VBO);
 
     // Save the current time --- it will be used to dynamically change the triangle color
     auto t_start = std::chrono::high_resolution_clock::now();
 
     // Register the keyboard callback
     glfwSetKeyCallback(window, key_callback);
+
+    //Register the mouse cursor position callback
+    glfwSetCursorPosCallback(window, cursor_position_callback);
 
     // Register the mouse callback
     glfwSetMouseButtonCallback(window, mouse_button_callback);
@@ -235,7 +263,8 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         // Bind your VAO (not necessary if you have only one)
-        VAO.bind();
+        line_VAO.bind();
+        tri_VAO.bind();
 
         // Bind your program
         program.bind();
@@ -249,7 +278,19 @@ int main(void)
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Draw a triangle
+
+        // Draw all complete triangles
+            //bind triangle VAO
+            //glBindVertexArray(tri_VAO);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+            //glDrawArrays(GL_TRIANGLES, 0, 3*num_triangles);
+            //glBindVertexArray(0);
+        
+        //if a line is being drawn
+            //bind line VAO
+            //glBindVertexArray(tri_VAO);
+            glDrawArrays(GL_LINE_STRIP,0,line_V.cols());
+            //glBindVertexArray(0);
 
         //if num_triangles > 0
         if(tri_complete)
@@ -264,7 +305,7 @@ int main(void)
         {
             //bind line VAO
             //glBindVertexArray(tri_VAO);
-            glDrawArrays(GL_LINE_STRIP,0,V.cols());
+            glDrawArrays(GL_LINE_STRIP,0,line_V.cols());
             //glBindVertexArray(0);
         }
         
@@ -279,8 +320,9 @@ int main(void)
 
     // Deallocate opengl memory
     program.free();
-    VAO.free();
-    VBO.free();
+    line_VAO.free();
+    line_VBO.free();
+    tri_VBO.free();
 
     // Deallocate glfw internals
     glfwTerminate();
